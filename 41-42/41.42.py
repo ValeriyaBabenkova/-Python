bca814aed3c7b16bc47cc00211d9ae9981b5cdf4
ValeriyaBabenkova <vlrbabenkova@gmail.com>
2024-09-19 20:48:45 +0500

    commit1909to2309

--- 41-42/41.42.py
+++ 41-42/41.42.py
@@ -1,3 +1,4 @@
+import random
 class Car:
     def __init__(self, color,fuel, consumption, kmage=0):
         self.color = color
@@ -13,7 +14,6 @@ class Car:
             print(f'Вы проехали {km} км')
             self.fuel -= need_fuel
             self.kmage += km
-            print(f'У вас осталось {self.fuel} л топлива')
         else:
             print('Вам не хватит топлива')
 
@@ -21,7 +21,37 @@ class Car:
     def get_mileage(self):
         return print(self.kmage)
 
-car = Car(color = 'синий', fuel = 20, consumption = 7)
+class SportCar(Car):
+    def fast_drive(self, km):
+        need_fuel = (km * self.consumption*1.5) / 100
+        if need_fuel <= self.fuel:
+            print(f'Вы проехали {km} км')
+            self.fuel -= need_fuel
+            self.kmage += km
+        else:
+            print('Вам не хватит топлива, чтобы доехать до места')
 
-car.drive(280)
-car.get_mileage()
+    def competition(self):
+        if random.randint(0, 1) == 0:
+            print('Ваша машина победила!!!')
+        else:
+            print('К сожалению, вы проиграли...')
+
+# car = Car(color = 'синий', fuel = 20, consumption = 7)
+#
+# car.drive(280)
+# car.get_mileage()
+
+car1 = Car(color='черный', fuel=8, consumption=8,kmage=0)
+car2 = SportCar(color='черный', fuel=8,consumption=8, kmage=0)
+
+print('\nПервая машина')
+for i in range(4):
+    car1.drive(30)
+
+
+print('\nВторая машина')
+for i in range(4):
+    car2.fast_drive(30)
+
+print(car2.competition())
---
+++ 43-44/43.44.py
@@ -0,0 +1,52 @@
+
+class Programmer:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+        self.hours = 0
+        self.money = 0
+        self.money_info = {'Junior': 10, 'Middle': 15, 'Senior': 20}
+
+
+    def work(self, time):
+        if self.grade == 'Junior':
+            self.hours += time
+            money = self.money_info['Junior']
+            self.money += time*money
+        if self.grade == 'Middle':
+            self.hours += time
+            money = self.money_info['Middle']
+            self.money += time * money
+        if self.grade == 'Senior':
+            self.hours += time
+            money = self.money_info['Senior']
+            self.money += time * money
+
+
+    def rise(self):
+        if self.grade == 'Junior':
+            self.grade = self.grade.replace('Junior', 'Middle')
+        elif self.grade == 'Middle':
+            self.grade = self.grade.replace('Middle', 'Senior')
+
+
+
+    def info(self):
+        print(f'{self.name} {self.hours} {self.money}тгр.')
+
+
+
+programmer = Programmer('Васильев Иван', 'Junior')
+programmer.work(750)
+print(programmer.info())
+programmer.rise()
+programmer.work(500)
+print(programmer.info())
+programmer.rise()
+programmer.work(250)
+print(programmer.info())
+programmer.rise()
+programmer.work(250)
+print(programmer.info())
+
+
