5bfc164a93c6dd42d58a61929f283c4a58e86de3
ValeriyaBabenkova <vlrbabenkova@gmail.com>
2024-10-12 21:02:15 +0500

    commit1210to1410

--- 51-52/51.52.py
+++ 51-52/51.52.py
@@ -1,25 +1,38 @@
 import random
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, wins=0):
         self.name = name
-        self.wins = 0
-        # print(self.name)
+        self.wins = wins
+    def __str__(self):
+        return self.name
+
 class Game:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+        self.wins1 = 0
+        self.wins2 = 0
+    def get_cubes(self):
+        return random.randint(1,6)+random.randint(1,6)
     def play_round(self):
-        result_player1 = random.randint(1,6)+random.randint(1,6)
-        print(f'{player1}выбросил {result_player1}')
-        result_player2 = random.randint(1,6)+random.randint(1,6)
-        print(f'{self.player2}выбросил {result_player2}')
+        self.result_player1 = self.get_cubes()
+        print(f'{player1} выбросил {self.result_player1}')
+        self.result_player2 = self.get_cubes()
+        print(f'{self.player2} выбросил {self.result_player2}')
 
-        if result_player1 > result_player2:
+        if self.result_player1 > self.result_player2:
+            self.wins1 += 1
             print(f'Победил {self.player1}')
-        elif result_player1 < result_player2:
+
+        elif self.result_player1 < self.result_player2:
+            self.wins2 += 1
             print(f'Победил {self.player2}')
         else:
             print('Ничья')
+
+
+    def get_statistic(self):
+        print(f'{self.player1} победил {self.wins1}\n{self.player2} победил {self.wins2}' )
 
 
 player1 = Player("Dima")
@@ -30,3 +43,6 @@ game = Game(player1, player2)
 
 game.play_round()
 game.play_round()
+game.play_round()
+
+game.get_statistic()
