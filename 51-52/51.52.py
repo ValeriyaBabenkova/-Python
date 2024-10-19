9d0cc45926678aa9511e7c3d3ed48ec321d9b63a
ValeriyaBabenkova <vlrbabenkova@gmail.com>
2024-10-19 19:54:00 +0500

    commit1910to1910

--- 51-52/51.52.py
+++ 51-52/51.52.py
@@ -10,8 +10,6 @@ class Game:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        self.wins1 = 0
-        self.wins2 = 0
     def get_cubes(self):
         return random.randint(1,6)+random.randint(1,6)
     def play_round(self):
@@ -21,18 +19,18 @@ class Game:
         print(f'{self.player2} выбросил {self.result_player2}')
 
         if self.result_player1 > self.result_player2:
-            self.wins1 += 1
+            self.player1.wins += 1
             print(f'Победил {self.player1}')
 
         elif self.result_player1 < self.result_player2:
-            self.wins2 += 1
+            self.player2.wins += 1
             print(f'Победил {self.player2}')
         else:
             print('Ничья')
 
 
     def get_statistic(self):
-        print(f'{self.player1} победил {self.wins1}\n{self.player2} победил {self.wins2}' )
+        print(f'{self.player1} победил {self.player1.wins}\n{self.player2} победил {self.player2.wins}' )
 
 
 player1 = Player("Dima")
